5b0d002a1ac7b4a7d07fc08926d805cbf7b3df35
apathoid <apathoid@tuta.io>
2025-10-16 13:27:55 +0300

    move to new eslint config api

--- index.cjs
+++ index.cjs
@@ -6,18 +6,56 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _globals = _interopRequireDefault(require("globals"));
 var _js = _interopRequireDefault(require("@eslint/js"));
-var _eslintPlugin = _interopRequireDefault(require("@typescript-eslint/eslint-plugin"));
-var _parser = _interopRequireDefault(require("@typescript-eslint/parser"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var _default = exports.default = [{
+var _typescriptEslint = _interopRequireDefault(require("typescript-eslint"));
+var _config = require("eslint/config");
+var _eslintPlugin = _interopRequireDefault(require("@stylistic/eslint-plugin"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+var _default = exports.default = (0, _config.defineConfig)([{
+  files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
   languageOptions: {
     globals: {
       ..._globals.default.browser,
       ..._globals.default.node
     }
+  }
+}, {
+  files: ["**/*.js"],
+  plugins: {
+    js: _js.default
+  },
+  extends: ["js/recommended"]
+}, {
+  plugins: {
+    '@stylistic': _eslintPlugin.default
   },
   rules: {
-    "semi": "error",
+    "semi": "off",
+    "@stylistic/semi": "error"
+  }
+}, _typescriptEslint.default.configs.recommended, {
+  plugins: {
+    '@typescript-eslint': _typescriptEslint.default.plugin
+  },
+  languageOptions: {
+    parser: _typescriptEslint.default.parser,
+    parserOptions: {
+      projectService: true,
+      ecmaFeatures: {
+        jsx: true
+      }
+    }
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", {
+      "vars": "local",
+      "varsIgnorePattern": "^_",
+      "argsIgnorePattern": "^_",
+      "destructuredArrayIgnorePattern": "^_",
+      "caughtErrors": "none"
+    }]
+  }
+}, {
+  rules: {
     "no-empty": ["error", {
       "allowEmptyCatch": true
     }],
@@ -52,34 +90,74 @@ var _default = exports.default = [{
       "allowTemplateLiterals": true
     }]
   }
-}, {
-  files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
-  rules: {
-    ..._js.default.configs.recommended.rules
-  }
-}, {
-  files: ["**/*.ts", "**/*.tsx"],
-  plugins: {
-    "@typescript-eslint": _eslintPlugin.default
-  },
-  rules: {
-    ..._eslintPlugin.default.configs.recommended.rules,
-    "@typescript-eslint/no-unused-vars": ["error", {
-      "vars": "local",
-      "varsIgnorePattern": "^_",
-      "argsIgnorePattern": "^_",
-      "destructuredArrayIgnorePattern": "^_",
-      "caughtErrors": "none"
-    }],
-    "semi": "off",
-    "@typescript-eslint/semi": "error"
-  },
-  languageOptions: {
-    parser: _parser,
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      }
-    }
-  }
-}];
+}]); // [
+//     {
+//         languageOptions: {
+//             globals: {
+//                 ...globals.browser,
+//                 ...globals.node
+//             }
+//         },
+//         rules: {
+//             "semi": "error",
+//             "no-empty": ["error", { "allowEmptyCatch": true }],
+//             "no-param-reassign": "error",
+//             "no-unsafe-optional-chaining": "warn",
+//             "prefer-const": ["warn", { "destructuring": "all" }],
+//             "no-else-return": "warn",
+//             "no-case-declarations": "warn",
+//             "no-useless-escape": "warn",
+//             "max-len": ["warn", 110],
+//             "no-return-assign": ["warn", "except-parens"],
+//             "no-eval": "error",
+//             "no-var": "warn",
+//             "no-constructor-return": "warn",
+//             "no-promise-executor-return": "warn",
+//             "no-trailing-spaces" : "warn",
+//             "function-paren-newline" : ["warn", "multiline-arguments"],
+//             "dot-location" : "warn",
+//             "no-tabs" : "warn",
+//             "no-multi-spaces" : "warn",
+//             "key-spacing" : "warn",
+//             "block-spacing" : "warn",
+//             "curly" : "warn",
+//             "nonblock-statement-body-position" : "warn",
+//             "new-parens" : "warn",
+//             "comma-style" : "warn",
+//             "no-self-compare" : "warn",
+//             "quotes": ["warn", "single", { "avoidEscape": true, "allowTemplateLiterals": true }]
+//         }
+//     },
+//     {
+//         files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
+//         rules: {
+//             ...js.configs.recommended.rules
+//         }
+//     },
+//     {
+//         files: ["**/*.ts", "**/*.tsx"],
+//         plugins: {
+//             "@typescript-eslint": tsPlugin
+//         },
+//         rules: {
+//             ...tsPlugin.configs.recommended.rules,
+//             "@typescript-eslint/no-unused-vars": ["error", {
+//                 "vars": "local",
+//                 "varsIgnorePattern": "^_",
+//                 "argsIgnorePattern": "^_",
+//                 "destructuredArrayIgnorePattern": "^_",
+//                 "caughtErrors": "none"
+//             }],
+//             "semi": "off",
+//             "@typescript-eslint/semi": "error"
+//         },
+//         languageOptions: {
+//             parser: tsParser,
+//             parserOptions: {
+//                 ecmaFeatures: {
+//                     jsx: true
+//                 }
+//             }
+//         }
+//     }
+// ];
--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-import globals from 'globals';
-
+import globals from "globals";
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
 
-
-export default [
+export default defineConfig([
     {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node
-            }
+        files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } }
+    },
+    {
+        files: ["**/*.js"],
+        plugins: { js },
+        extends: ["js/recommended"]
+    },
+    {
+        plugins: {
+            '@stylistic': stylistic
         },
         rules: {
-            "semi": "error",
+            "semi": "off",
+            "@stylistic/semi": "error"
+        }
+    },
+    tseslint.configs.recommended,
+    {
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["error", {
+                "vars": "local",
+                "varsIgnorePattern": "^_",
+                "argsIgnorePattern": "^_",
+                "destructuredArrayIgnorePattern": "^_",
+                "caughtErrors": "none"
+            }],
+        },
+    },
+    {
+        rules: {
             "no-empty": ["error", { "allowEmptyCatch": true }],
             "no-param-reassign": "error",
             "no-unsafe-optional-chaining": "warn",
@@ -29,51 +63,19 @@ export default [
             "no-var": "warn",
             "no-constructor-return": "warn",
             "no-promise-executor-return": "warn",
-            "no-trailing-spaces" : "warn",
-            "function-paren-newline" : ["warn", "multiline-arguments"],
-            "dot-location" : "warn",
-            "no-tabs" : "warn",
-            "no-multi-spaces" : "warn",
-            "key-spacing" : "warn",
-            "block-spacing" : "warn",
-            "curly" : "warn",
-            "nonblock-statement-body-position" : "warn",
-            "new-parens" : "warn",
-            "comma-style" : "warn",
-            "no-self-compare" : "warn",
+            "no-trailing-spaces": "warn",
+            "function-paren-newline": ["warn", "multiline-arguments"],
+            "dot-location": "warn",
+            "no-tabs": "warn",
+            "no-multi-spaces": "warn",
+            "key-spacing": "warn",
+            "block-spacing": "warn",
+            "curly": "warn",
+            "nonblock-statement-body-position": "warn",
+            "new-parens": "warn",
+            "comma-style": "warn",
+            "no-self-compare": "warn",
             "quotes": ["warn", "single", { "avoidEscape": true, "allowTemplateLiterals": true }]
         }
-    },
-    {
-        files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
-        rules: {
-            ...js.configs.recommended.rules
-        }
-    },
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-        plugins: {
-            "@typescript-eslint": tsPlugin
-        },
-        rules: {
-            ...tsPlugin.configs.recommended.rules,
-            "@typescript-eslint/no-unused-vars": ["error", {
-                "vars": "local",
-                "varsIgnorePattern": "^_",
-                "argsIgnorePattern": "^_",
-                "destructuredArrayIgnorePattern": "^_",
-                "caughtErrors": "none"
-            }],
-            "semi": "off",
-            "@typescript-eslint/semi": "error"
-        },
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true
-                }
-            }
-        }
     }
-];
+]);
